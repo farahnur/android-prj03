345d6ab75bb690130aa4abfa456a18b29ff91fdf
farahnur <farah.nurazzahra@gmail.com>
2019-10-19 09:28:58 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/dts/farah/prj03/MainActivity.java
@@ -0,0 +1,44 @@
+package id.ac.poliban.dts.farah.prj03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //mendeklarasikan listview var dan menginisialisasi array tipe data string
+    private ListView lvItem;
+    private String[] namanegara = new String[]{
+            "Indonesia", "Malaysia", "Singapore",
+            "Italia", "Inggris", "Belanda",
+            "Argentina", "Chile",
+            "Mesir", "Uganda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        getSupportActionBar().setTitle("ListView Sederhana"); //tampil judul
+
+        //membinding atau memformat data
+        lvItem = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        //menset data di dalam listview
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namanegara[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
